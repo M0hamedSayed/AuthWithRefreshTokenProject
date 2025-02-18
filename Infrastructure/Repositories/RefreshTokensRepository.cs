f625ebd15ded1888371241a199e46ac749f2ba36
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-19 00:03:05 +0200

    feat : customize repoistory pattern with unit of work

---
+++ Infrastructure/Repositories/RefreshTokensRepository.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities.Identity;
+using Infrastructure.Base;
+using Infrastructure.DatabaseContext;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class RefreshTokensRepository : GenericRepositoryAsync<UserRefreshTokens>, IRefreshTokensRepository
+    {
+        private readonly DbSet<UserRefreshTokens> _refreshTokens;
+
+        public RefreshTokensRepository(ApplicationDbContext context) : base(context)
+        {
+            _refreshTokens = context.Set<UserRefreshTokens>();
+        }
+    }
+}
