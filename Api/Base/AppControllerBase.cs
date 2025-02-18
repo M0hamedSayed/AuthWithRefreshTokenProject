4ce6f1fb160ac8aea71f788474070f97c036ee97
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-18 14:01:59 +0200

    feat : handle custom controlBase

---
+++ Api/Base/AppControllerBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Core.Base;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppControllerBase : ControllerBase
+    {
+        private IMediator? _mediator;
+        protected IMediator Mediator =>
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+                ?? throw new InvalidOperationException("IMediator service is unavailable");
+    
+        public ObjectResult HandleResult<T> (Response<T> result)
+        {
+            Dictionary<string, object?>? response = result.ToNonNullDictionary();
+
+            switch (result.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    return new OkObjectResult(response);
+                case HttpStatusCode.Created:
+                    return new CreatedResult(string.Empty, response);
+                case HttpStatusCode.Unauthorized:
+                    return new UnauthorizedObjectResult(response);
+                case HttpStatusCode.BadRequest:
+                    return new BadRequestObjectResult(response);
+                case HttpStatusCode.NotFound:
+                    return new NotFoundObjectResult(response);
+                case HttpStatusCode.Accepted:
+                    return new AcceptedResult(string.Empty, response);
+                case HttpStatusCode.UnprocessableEntity:
+                    return new UnprocessableEntityObjectResult(response);
+                default:
+                    return new BadRequestObjectResult(response);
+            }
+        }
+    }
+}
