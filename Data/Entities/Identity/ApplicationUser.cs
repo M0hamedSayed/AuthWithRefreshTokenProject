5b3c01aba4b682e6a06d315ac74a0c823c933f23
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-18 14:04:32 +0200

    feat : update user tables

--- Data/Entities/Identity/ApplicationUser.cs
+++ Data/Entities/Identity/ApplicationUser.cs
@@ -1,19 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Data.Interfaces;
 using Microsoft.AspNetCore.Identity;
 
 namespace Data.Entities.Identity
 {
-    public class ApplicationUser : IdentityUser<int>, ISoftDeletable
+    public class ApplicationUser : IdentityUser<int>, ISoftDeletable, ITableCreation
     {
+        [MaxLength(100)]
+        [Column("full_name")]
         public required String FullName { get; set; }
-        public string? Address { get; set; }
-        public string? Country { get; set; }
-        public DateTime? CreatedAt { get; set; } = null;
-        public DateTime? UpdatedAt { get; set; } = null;
-        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        [Column("deleted_at")]
         public DateTime? DeletedAt { get; set; } = null;
-
+        [Column("is_deleted")]
+        public bool IsDeleted { get; set; } = false;
         [InverseProperty(nameof(UserRefreshTokens.user))]
         public virtual ICollection<UserRefreshTokens>? userRefreshTokens { get; set; }
     }
--- Data/Entities/Identity/UserRefreshTokens.cs
+++ Data/Entities/Identity/UserRefreshTokens.cs
@@ -18,12 +18,38 @@ namespace Data.Entities.Identity
         public required string AccessToken { get; set; }
         [Column("refresh_token")]
         public required string RefreshToken { get; set; }
+        [Column("is_used")]
         public bool IsUsed { get; set; } = false;
+        [Column("is_revoked")]
         public bool IsRevoked { get; set; } = false;
         [Column("added_time")]
-        public DateTime AddedTime { get; set; }
+        public DateTime AddedTime { get; set; } = DateTime.UtcNow;
         [Column("expiry_date")]
         public DateTime ExpiryDate { get; set; }
+
+        // user metadata
+        [Column("user_agent",TypeName = "nvarchar(300)")]
+        public string UserAgent { get; set; } = string.Empty;
+        [MaxLength(20)]
+        public string IP { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string? Country { get; set; }
+        [MaxLength(10)]
+        [Column("country_code")]
+        public string? CountryCode { get; set; }
+        [MaxLength(100)]
+        public string? City { get; set;}
+        [MaxLength(100)]
+        [Column("time_zone")]
+        public string? TimeZone { get; set; }
+        [Precision(8, 6)]
+        [Column("location_lat")]
+        public decimal? LocationLat { get; set; }
+        [Precision(9, 6)]
+        [Column("location_lng")]
+        public decimal? LocationLng { get; set; }
+
 
         [ForeignKey(nameof(UserId))]
         [InverseProperty(nameof(ApplicationUser.userRefreshTokens))]
