5c1ce824f565b35f0dbb1d8084da0f29fe1efb2c
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-19 00:03:58 +0200

    feat : customize generic repository

---
+++ Infrastructure/Base/GenericRepositoryAsync.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Consts;
+using Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Infrastructure.Base
+{
+    public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
+    {
+        #region Fields
+        protected readonly ApplicationDbContext _context;
+        #endregion
+        #region Constructor
+        public GenericRepositoryAsync(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<T> AddAsync(T entity)
+        {
+             await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+            return entities;
+        }
+
+        
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<T>().CountAsync();
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> criteria)
+        {
+            return await _context.Set<T>().CountAsync(criteria);
+        }
+
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[]? includes)
+        {
+            return await HandleIncludes(includes).Where(criteria).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[]? includes, int take, int skip)
+        {
+            return await HandleIncludes(includes).Where(criteria).Skip(skip).Take(take).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[]? includes, int? take, int? skip, Expression<Func<T, object>>? orderBy, string orderByDirection = OrderBy.Ascending)
+        {
+            IQueryable<T> query = HandleIncludes(includes).Where(criteria);
+
+            if (take.HasValue)
+                query = query.Take(take.Value);
+
+            if (skip.HasValue)
+                query = query.Skip(skip.Value);
+
+            if (orderBy != null)
+            {
+                if (orderByDirection == OrderBy.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> FindAsync(Expression<Func<T, bool>> criteria, string[]? includes)
+        {
+            return await HandleIncludes(includes).SingleOrDefaultAsync(criteria);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(int skip, int take)
+        {
+            return await _context.Set<T>().Skip(skip).Take(take).ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public IQueryable<T> GetTableAsTracking()
+        {
+            return _context.Set<T>().AsQueryable();
+        }
+
+        public IQueryable<T> GetTableNoTracking()
+        {
+            return _context.Set<T>().AsNoTracking().AsQueryable();
+        }
+
+        public T Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            return entity;
+        }
+
+        public IEnumerable<T> UpdateRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().UpdateRange(entities);
+            return entities;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+
+        public IDbContextTransaction BeginTransaction()
+        {
+            return _context.Database.BeginTransaction();
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await _context.Database.BeginTransactionAsync();
+        }
+
+        public void Commit()
+        {
+            _context.Database.CommitTransaction();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.Database.CommitTransactionAsync();
+        }
+
+        public void RollBack()
+        {
+            _context.Database.RollbackTransaction();
+        }
+
+        public async Task RollBackAsync()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+
+        private IQueryable<T> HandleIncludes(string[]? includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes is not null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+            return query;
+        }
+        #endregion
+    }
+}
---
+++ Infrastructure/Base/IGenericRepositoryAsync.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Consts;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Infrastructure.Base
+{
+    public interface IGenericRepositoryAsync<T> where T : class
+    {
+        // tracking
+        IQueryable<T> GetTableNoTracking();
+        IQueryable<T> GetTableAsTracking();
+        // get
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(int skip, int take);
+
+        Task<T?> GetByIdAsync(int id);
+        //find
+        Task<T?> FindAsync(Expression<Func<T, bool>> criteria, string[]? includes);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[]? includes);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[]? includes, int take, int skip);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[]? includes, int? take, int? skip,
+            Expression<Func<T, object>>? orderBy, string orderByDirection = OrderBy.Ascending);
+        //add
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        // update
+        T Update(T entity);
+        IEnumerable<T> UpdateRange(IEnumerable<T> entities);
+        // delete
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+        // count
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<T, bool>> criteria);
+        // save changes
+        Task SaveChangesAsync();
+
+        // transactions
+        IDbContextTransaction BeginTransaction();
+        void Commit();
+        void RollBack();
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollBackAsync();
+
+    }
+}
