f525e5dbc556589b783f1c85022e5237e951e884
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-18 14:04:59 +0200

    feat : handle custom response object

---
+++ Core/Base/Response.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Base
+{
+    public class Response<T>
+    {
+        #region Fields
+        public HttpStatusCode? StatusCode { get; set; } = HttpStatusCode.InternalServerError;
+        public object? Meta { get; set; }
+
+        public bool? Succeeded { get; set; }
+        public string? Message { get; set; }
+        public List<string>? Errors { get; set; }
+        public T? Data { get; set; }
+        #endregion
+
+        #region Constructors
+        public Response()
+        {
+            
+        }
+
+        public Response(T data, string message)
+        {
+            Data = data;
+            Message = message;
+        }
+
+        public Response(string message)
+        {
+            Message = message;
+        }
+
+        public Response(string message, bool succeeded)
+        {
+            Succeeded = succeeded;
+            Message = message;
+        }
+        #endregion
+
+        #region Methods
+        // Method to return non-null fields as a dictionary
+        public Dictionary<string, object?> ToNonNullDictionary()
+        {
+            var nonNullFields = new Dictionary<string, object?>();
+
+            if (StatusCode != null)
+                nonNullFields.Add(nameof(StatusCode), StatusCode);
+            else nonNullFields.Add(nameof(StatusCode), 500);
+            if (Meta != null)
+                nonNullFields.Add(nameof(Meta), Meta);
+            if (Succeeded != null)
+                nonNullFields.Add(nameof(Succeeded), Succeeded);
+            if (Message != null)
+                nonNullFields.Add(nameof(Message), Message);
+            if (Errors != null && Errors.Count > 0)
+                nonNullFields.Add(nameof(Errors), Errors);
+            if (Data != null)
+                nonNullFields.Add(nameof(Data), Data);
+
+            return nonNullFields;
+        }
+        #endregion
+    }
+}
---
+++ Core/Base/ResponseHandler.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+
+namespace Core.Base
+{
+    public class ResponseHandler
+    {
+        public ResponseHandler() {}
+
+        #region Methods
+        public Response<T> Deleted<T>(string? Message)
+        {
+            return new Response<T>()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Succeeded = true,
+                Message = Message is null ? "Deleted Successfully" : Message
+            };
+        }
+        public Response<T> Success<T>(T entity, object? Meta)
+        {
+            return new Response<T>()
+            {
+                Data = entity,
+                StatusCode = HttpStatusCode.OK,
+                Succeeded = true,
+                Message = "Data Returned Successfully",
+                Meta = Meta
+            };
+        }
+        public Response<T> Unauthorized<T>(string? Message)
+        {
+            return new Response<T>()
+            {
+                StatusCode = HttpStatusCode.Unauthorized,
+                Succeeded = true,
+                Message = Message is null ? "You are not Authorized To Access This Resources" : Message
+            };
+        }
+        public Response<T> BadRequest<T>(string? Message)
+        {
+            return new Response<T>()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Succeeded = false,
+                Message = Message is null ? "Request Can't be Understand" : Message
+            };
+        }
+
+        public Response<T> UnprocessableEntity<T>(string? Message)
+        {
+            return new Response<T>()
+            {
+                StatusCode = HttpStatusCode.UnprocessableEntity,
+                Succeeded = false,
+                Message = Message is null ? "There are Validation Errors Or Syntax Errors" : Message
+            };
+        }
+
+
+        public Response<T> NotFound<T>(string? message)
+        {
+            return new Response<T>()
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Succeeded = false,
+                Message = message is null ? "Not Found" : message
+            };
+        }
+
+        public Response<T> Created<T>(T entity, object Meta = null)
+        {
+            return new Response<T>()
+            {
+                Data = entity,
+                StatusCode = HttpStatusCode.Created,
+                Succeeded = true,
+                Message = "Created Successfully",
+                Meta = Meta
+            };
+        }
+        #endregion
+    }
+}
