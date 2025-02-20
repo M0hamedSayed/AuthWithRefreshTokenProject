74d333e55513118ab145866addc1c4fca8d7d306
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-20 14:48:42 +0200

    feat : auth mapping

---
+++ Core/Mapping/Authentication/AuthenticationProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace Core.Mapping.Authentication
+{
+    public partial class AuthenticationProfile: Profile
+    {
+        public AuthenticationProfile()
+        {
+            CreateUserMapping();
+            LoginMapping();
+        }
+    }
+}
