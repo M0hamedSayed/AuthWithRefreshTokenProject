8e4aba6331cd47187909ee165976c678bf6fc575
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-20 23:25:31 +0200

    feat : user endpoints

---
+++ Api/Controllers/v1/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Api.Base;
+using Core.Features.User.Queries.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.v1
+{
+    [Authorize(Roles = "Admin,User")]
+    public class UserController : AppControllerBase
+    {
+        [HttpGet("me")]
+        public async Task<IActionResult> GetUser()
+        {
+            var res = await Mediator.Send(new GetMeQuery());
+            return HandleResult(res);
+        }
+    }
+}
