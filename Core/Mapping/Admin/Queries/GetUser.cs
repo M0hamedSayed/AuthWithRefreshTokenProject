3ed7b45fb3e16c7bfff41d23259658d3f58af26b
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-20 23:25:51 +0200

    feat : handle mapping response

---
+++ Core/Mapping/Admin/Queries/GetUser.cs
@@ -0,0 +1,16 @@
+﻿using Core.Features.Admin.Queries.Results;
+using Data.Entities.Identity;
+
+namespace Core.Mapping.Admin
+{
+    public partial class AdminProfile
+    {
+        public void UserMapping()
+        {
+            CreateMap<UserRefreshTokens, ActiveUserSession>();
+
+            CreateMap<ApplicationUser, GetUserResult>()
+                .ForMember(dest => dest.activeSessions, opt => opt.MapFrom( src => src.userRefreshTokens));
+        }
+    }
+}
---
+++ Core/Mapping/User/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Core.Mapping.User
+{
+    public partial class UserProfile : Profile
+    {
+        public UserProfile() 
+        {
+            UserMapping();
+        }
+    }
+}
