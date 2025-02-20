44572b6d4f7b593687baeec6d2c8796e49c9399e
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-20 23:23:38 +0200

    feat : handle admin only filter

---
+++ Core/Filters/AdminOnlyFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Service.Interfaces;
+
+namespace Core.Filters
+{
+    public class AdminOnlyFilter :IAsyncActionFilter
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ICurrentUserService _currentUserService;
+
+        public AdminOnlyFilter(IUnitOfWork unitOfWork, ICurrentUserService currentUserService)
+        {
+            _unitOfWork = unitOfWork;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if(context?.HttpContext?.User?.Identity?.IsAuthenticated == true)
+            {
+                var roles = await _currentUserService.GetCurrentUserRolesAsync();
+                if(roles.Any(x => x is "Admin")) await next();
+                else context.Result = new ObjectResult("Forbidden!") { StatusCode = 403 };
+            }
+        }
+    }
+}
