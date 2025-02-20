915968ea5d9135875bf3119b72a5d4194539e798
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-02-20 14:54:40 +0200

    feat : inject services and handle rateLimiter

---
+++ Service/Extensions/ModuleServicesDependencies.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCoreRateLimit;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Implementations;
+using Service.Interfaces;
+
+namespace Service.Extensions
+{
+    public static class ModuleServicesDependencies
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            // add rate limiting
+            services.AddMemoryCache();
+            services.Configure<IpRateLimitOptions>(options =>
+                {
+                    options.GeneralRules = new List<RateLimitRule>
+                {
+                    new RateLimitRule
+                    {
+                        Endpoint = "*",
+                        Period = "1m",  // Limit to 30 request per 1 minutes
+                        Limit = 30
+                    }
+                };
+            });
+            services.AddInMemoryRateLimiting();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            // dependancy injections
+            services.AddTransient<IEmailService,EmailService>();
+            services.AddTransient<IAuthenticationService,AuthenticationService>();
+            services.AddTransient<ICurrentUserService, CurrentUserService>();
+
+            return services;
+        }
+    }
+}
